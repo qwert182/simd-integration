2146a051e935afc0c204eb3f2b3fc2cdb1e25edb
qwert182 <qwert182@users.noreply.github.com>
2021-10-04 20:18:53 +0300

    Always use fast SSE sqrt in semicircle()

--- src/functions.c
+++ src/functions.c
@@ -1,9 +1,12 @@
 #include "functions.h"
-#include <math.h>
+//#include <math.h>
+#include <immintrin.h>
 
 /*
  * https://en.wikipedia.org/wiki/Semicircle#Equation
  */
 double semicircle(double x) {
-    return sqrt(1.0 - x*x);
+    //return sqrt(1.0 - x*x);
+    __m128d s = _mm_set_sd(1.0 - x*x);
+    return _mm_cvtsd_f64(_mm_sqrt_sd(s, s));
 }
